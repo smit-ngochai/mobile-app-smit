077ccc2f66cee9f9709974b1218870d1d6f881fd
Trương Ngọc Hải <157113567+smit-ngochai@users.noreply.github.com>
2025-05-07 10:24:02 +0700

    update tăng time màn hình chờ cho ios

--- ios/AppSmit/AppDelegate.swift
+++ ios/AppSmit/AppDelegate.swift
@@ -9,17 +9,38 @@ class AppDelegate: RCTAppDelegate {
   // Thêm biến static để lưu trạng thái orientation
   static var orientationLock = UIInterfaceOrientationMask.all
   
+  // Thêm window tùy chỉnh
+  var launchScreenWindow: UIWindow?
+  
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-
+    
+    // Chuẩn bị React Native
     self.moduleName = "AppSmit"
     self.dependencyProvider = RCTAppDependencyProvider()
-
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    
+    // Khởi tạo React Native (bước này sẽ tạo window chính và bắt đầu tải JS bundle)
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    
+    // Tạo một window mới cho launch screen
+    launchScreenWindow = UIWindow(frame: UIScreen.main.bounds)
+    if let window = launchScreenWindow {
+      window.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+      window.windowLevel = .alert + 1 // Đặt cao hơn window chính
+      window.makeKeyAndVisible()
+    }
+    
+    // Sau 3 giây, ẩn launch screen window
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      UIView.animate(withDuration: 0.3, animations: {
+        self.launchScreenWindow?.alpha = 0
+      }, completion: { _ in
+        self.launchScreenWindow = nil
+      })
+    }
+    
+    return result
   }
 
   override func sourceURL(for bridge: RCTBridge) -> URL? {
